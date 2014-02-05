59a8eef0ab4f3b22275f5d7134ce10fddd724b31
Matt <m4tt1muspr1m3@gmail.com>
2014-02-05 16:45:03 -0600

    init commit
    this is where Glenn and I left off in the interview

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public IEnumerable<string> Run()
+        {
+            return Run(1, 100);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public IEnumerable<string> Run(int start, int end)
+        {
+            var result = new List<string>();
+
+            var diff = end - start;
+            if (diff > 1000)
+                return result;
+
+            for (int i = start; i < end + 1; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Interview/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Interview
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fb = new FizzBuzz.FizzBuzz();
+            IEnumerable<string> outputs = fb.Run(100, 1);
+            foreach (var output in outputs)
+            {
+                Console.WriteLine(output);
+            }
+            Console.ReadKey();
+        }
+    }
+}
