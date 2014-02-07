6e5f4cca29a9d812fbf0fb289f218ff4d51a93e1
Matt <m4tt1muspr1m3@gmail.com>
2014-02-06 18:19:35 -0600

    Added tests

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    public class FizzBuzz
+    public class Program
     {
         public IEnumerable<string> Run()
         {
--- Interview.Tests/FizzBuzzTests.cs
+++ Interview.Tests/FizzBuzzTests.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using FizzBuzz;
 
 namespace Interview.Tests
 {
+    [TestFixture]
     public class FizzBuzzTests
     {
+        [Test]
+        public void BasicNumberTest()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(1, 2).ToArray();
+
+            Assert.AreEqual("1", result[0]);
+            Assert.AreEqual("2", result[1]);
+        }
+
+        [Test]
+        public void TestFizz()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(1, 3).ToArray().Last();
+
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void TestBuzz()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(1, 5).ToArray().Last();
+
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void TestFizzBuzz()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(1, 15).ToArray().Last();
+
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [Test]
+        public void TestLowerStartNumberThenEndNumber()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(12, 1);
+
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void OneBillionRangeTest()
+        {
+            var fb = new Program();
+
+            var result = fb.Run(1, 1000000001);
+
+            Assert.AreSame(1000000001, result.Count());
+        }
     }
 }
